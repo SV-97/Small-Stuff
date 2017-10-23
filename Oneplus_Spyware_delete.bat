5d9fdbfbd4e652f83d1a0b3bc966ac69286b575b
SV-97 <volzstefan97@googlemail.com>
2017-10-23 19:02:42 +0200

    Add files via upload

---
+++ Oneplus_Spyware_delete.bat
@@ -0,0 +1,14 @@
+@echo off
+Color 0A
+set /p pfad=Pfad Hauptverzeichnis adb(Standart C:\adb) 
+if defined %pfad% (GOTO:EOF)
+if %pfad%==nul ( set %pfad% = "C:\adb" )
+cd %pfad%
+adb devices
+echo "ggf. Verbindung am Handy akzeptieren"
+timeout /T 10
+adb devices
+adb shell
+pm unistall -k --user 0 net.oneplus.odm
+exit
+exit
---
+++ SleepShutdown.bat
@@ -0,0 +1,19 @@
+@echo off
+Color 0A
+echo %USERNAME% on %COMPUTERNAME%
+set /p frage=Sleep Timer starten? (y/n)
+if %frage%==n (GOTO:EOF)
+set /p time=Bitte Sleep-Zeit eintragen(suffix *s*econds, *M*inutes, *h*ours)
+
+If %time:~-1%==s set factor=1 && set nosuffix=false  || set nosuffix=true
+If %time:~-1%==m set factor=60 && set nosuffix=false  || set nosuffix=true
+If %time:~-1%==h set factor=360 && set nosuffix=false || set nosuffix=true
+
+If %nosuffix%==true set factor=60 ELSE (echo schoki)
+
+set /A timeS=%time:~0,-1%*%factor%
+%time:time=timeS%
+
+REM timeout /T /NOBREAK %time%
+if %frage%==y (Shutdown /s /t %time%)
+pause >nul
