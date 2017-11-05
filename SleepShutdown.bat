5b441c58a53df54d46bad13ba8e940f6ef727327
SV-97 <volzstefan97@googlemail.com>
2017-11-05 20:07:57 +0100

    Fixed Script

--- SleepShutdown.bat
+++ SleepShutdown.bat
@@ -3,15 +3,18 @@ set /p frage=Sleep Timer starten? (y/n)
 if %frage%==n (GOTO:EOF)
 set /p time=Bitte Sleep-Zeit eintragen(suffix *s*econds, *M*inutes, *h*ours)
 
-If %time:~-1%==s set factor=1 && set nosuffix=false  || set nosuffix=true
+REM if the last character of time is "s", set factor to 1, nosuffix to false; if statement not true set nosuffix to true
+If %time:~-1%==s set factor=1 && set nosuffix=false  || set nosuffix=true 
 If %time:~-1%==m set factor=60 && set nosuffix=false  || set nosuffix=true
 If %time:~-1%==h set factor=360 && set nosuffix=false || set nosuffix=true
+
+echo %time%
 
 If %nosuffix%==true set factor=60 ELSE (echo error)
 
 set /A timeS=%time:~0,-1%*%factor%
-%time:time=timeS%
+REM %time:time=timeS%
 
 REM timeout /T /NOBREAK %time%
-if %frage%==y (Shutdown /s /t %time%)
+if %frage%==y (Shutdown /s /t %timeS%)
 pause >nul
